8eb2dc46b43aad1db7f027c1ad1857d17ee8a6c8
Kyr001 <cuppastorm@gmail.com>
2024-05-27 11:49:05 +0900

    1~4

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float jumpPower;
+    private Vector2 curMovementInput;
+    public LayerMask groundLaterMask;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    //public bool canLook = true;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //커서 보이지 않게 해줌
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() //물리연산
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        //if(canLook)
+        {
+            CameraLook();
+        }
+    }
+
+ 
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed; //움직이는 힘을 곱해줌
+        dir.y = _rigidbody.velocity.y; // 점프할 때만 y값이 변함
+
+        _rigidbody.velocity = dir;
+    
+    }
+
+    void CameraLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+       if(context.phase == InputActionPhase.Performed) // 키가 눌릴 때 Started, 키가 눌린 후에도 계속 호출하려면 Performed
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+       else if(context.phase == InputActionPhase.Canceled) // 액션 취소됐을 때
+        {
+            curMovementInput = Vector2.zero;
+
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started && IsGrounded())
+        {
+            _rigidbody.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    // 플레이어가 바닥에 붙어있는지 확인 
+
+    bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
+        };
+
+        for(int i = 0; i<rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLaterMask))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
