b5893dd7b58ef1b90bf0d6de3048955ef68ef2d1
Kyr001 <cuppastorm@gmail.com>
2024-05-29 17:39:29 +0900

    init

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
+    [Range(0.0f, 1.0f)]
+    public float time;
+    public float fullDayLength;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon; // Vector 90 0 0
+
+    [Header("Sun")]
+    public Light sun;
+    public Gradient suncolor;
+    public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient mooncolor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Other Lighting")]
+    public AnimationCurve lightintensityMultiplier;
+    public AnimationCurve reflectionintenstyMuliplier;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
     }
 
     // Update is called once per frame
